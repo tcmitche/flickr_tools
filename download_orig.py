7745b2347eb32028e7222deed20b4cf1816b30f7
Travis Mitchell <contact@traviscmitchell.com>
2014-10-18 17:16:10 -0700

    Successfully downloaded a photo.

--- download_orig.py
+++ download_orig.py
@@ -2,6 +2,7 @@
 import flickr
 import urllib
 import ConfigParser
+import os
 
 config = ConfigParser.ConfigParser()
 config.read('conf')
@@ -10,10 +11,17 @@ flickr.API_KEY = config.get('ApiCredentials', 'key')
 flickr.API_SECRET = config.get('ApiCredentials', 'secret')
 
 setId = '72157648278345450'
+photoDir = 'photos'
+
+if not os.path.exists(photoDir):
+    os.makedirs(photoDir)
 
 print('Calling flickr for set: %s' % setId)
 photos = flickr.Photoset(setId).getPhotos()
 
 # for p in photos:
 p = photos[0]
-print('https://farm%s.staticflickr.com/%s/%s_%s_o.%s' % (p.farm, p.server, p.id, p.originalsecret, p.originalformat))
+pUrl = 'https://farm%s.staticflickr.com/%s/%s_%s_o.%s' % (p.farm, p.server, p.id, p.originalsecret, p.originalformat)
+f = open('%s/%s.%s' % (photoDir, p.id, p.originalformat), 'wb')
+f.write(urllib.urlopen(pUrl).read())
+f.close()
